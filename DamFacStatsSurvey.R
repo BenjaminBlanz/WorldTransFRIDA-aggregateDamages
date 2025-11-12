f80a597c98d1f5a19ad58f4beea5954e56209f45
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2025-11-12 14:30:29 +0100

    changes up to the figures

--- DamFacStatsSurvey.R
+++ DamFacStatsSurvey.R
@@ -12,6 +12,7 @@ library(doParallel); library(foreach)
 
 # Filter for Collinear Variables (one of two is randomly dropped) ###################################
 
+# (We can increase the cutoff value to 0.95 (cutoff=0.99) if we want more variables in the data frame.)
 cutoffPar <- 0.2
 
 DamFuc <- readRDS("data/asRegDF.RDS")
@@ -19,7 +20,7 @@ DamFuc  <- DamFuc[,-which(colnames(DamFuc)%in%c('gdpGro','gdp_future_recession_c
 
 num_ok  <- sapply(DamFuc, is.numeric) & sapply(DamFuc, sd, na.rm = TRUE) > 0 # We keep numeric vectors and sd greater zero
 R       <- cor(DamFuc[, num_ok, drop = FALSE], use = "pairwise.complete.obs")
-drops   <- setdiff(findCorrelation(R, cutoff = cutoffPar, names = TRUE), # (We can increase the cutoff value to 0.95 (cutoff=0.99) if we want more variables in the data frame.)
+drops   <- setdiff(findCorrelation(R, cutoff = cutoffPar, names = TRUE), 
                    c("gdp","l1gdp","sta","id","year","l1gdp","l1sta",
                    	"l2gdp","l2sta","l3gdp","l3sta","l10gdp","l10sta",
                    	"l20gdp","l20sta","l30gdp","l30sta","l40gdp","l40sta")) # If we want to keep more variables for sure, we can add those here.
@@ -68,7 +69,7 @@ fml_base   <- mk_formula(y, c)
 
 anchor_vars <- intersect(c, c("sta","sta2","l1gdp")) # These variables are always part of the model. We can include those we want to have for sure.
 pool        <- setdiff(c, anchor_vars)
-max_k <- min(5L, length(pool))  # I set it to 3, but in Levante we can set it higher. We have to be careful, because of the exponential combinatorics.
+max_k <- min(6L, length(pool))  # I set it to 3, but in Levante we can set it higher. We have to be careful, because of the exponential combinatorics.
 
 rhs_list <- unique(c(if (length(anchor_vars)) paste(anchor_vars, collapse = " + ") else NULL, unlist(lapply(1:max_k, function(k)
     apply(combn(pool, k), 2, function(cols) paste(c(anchor_vars, cols), collapse = " + "))), use.names = FALSE)))
@@ -85,16 +86,26 @@ chunkSize <- 100
 
 parfun <- function(i){
 	rhs <- rhs_list[[i]]
-	m <- tryCatch(
-		plm(as.formula(paste(y, "~", rhs)), data = pdata, model = "pooling"),  # We can change the model to FE or RE etc. and adjust it to the other methods
-		error = function(e) NULL)
-	if (is.null(m)) return(NULL)
-	r <- residuals(m)
-	n <- sum(!is.na(r))
-	rss <- sum(r^2, na.rm = TRUE)
-	k <- length(coef(m))
-	# list(rhs = rhs, model = m, bic = n*log(rss/n) + k*log(n))
-	return(list(bic= -n*log(rss/n)+k*log(n)))
+	sucess <- F
+	tryCatch({
+		m <- plm(as.formula(paste(y, "~", rhs)), data = pdata, model = "pooling") # We can change the model to FE or RE etc. and adjust it to the other methods
+		r <- residuals(m)
+		n <- sum(!is.na(r))
+		rss <- sum(r^2, na.rm = TRUE)
+		tss <- sum((pdata[[y]] - mean(pdata[[y]]))^2)
+		k <- length(coef(m))
+		# list(rhs = rhs, model = m, bic = n*log(rss/n) + k*log(n))
+		bic <- -n*log(rss/n)+k*log(n)
+		r2 <- 1-rss/tss
+		adjr2 <- 1-((1-r2)*(n-1))/(n-k-1)
+		sucess <- T
+		},  
+		error = function(e){})
+	if (sucess) {
+		return(list(i=i,bic=bic,r2=r2,adjr2=adjr2))
+	} else {
+		return(NULL)
+	}
 }
 
 cl <- makeForkCluster(min(detectCores(),length(rhs_list)/chunkSize))
@@ -119,40 +130,39 @@ stopifnot(length(fits) > 0L)
 
 # The "best of all" models is selected
 bics <- vapply(fits, `[[`, numeric(1), "bic")
-best.idx <- which.min(bics)
+r2s <- vapply(fits, `[[`, numeric(1), "r2")
+adjr2s <- vapply(fits, `[[`, numeric(1), "adjr2")
+best.idx.bic <- which.min(bics)
+best.idx.bic <- fits[[best.idx.bic]]$i
+best.idx.r2 <- which.max(r2s)
+best.idx.r2 <- fits[[best.idx.r2]]$i
+best.idx.adjr2 <- which.max(adjr2s)
+best.idx.adjr2 <- fits[[best.idx.adjr2]]$i
+bestModels <- list()
+bestModels$r2 <- plm(as.formula(paste(y, "~", rhs_list[[best.idx.r2]])), data = pdata, model = "pooling")
+bestModels$adjr2 <- plm(as.formula(paste(y, "~", rhs_list[[best.idx.adjr2]])), data = pdata, model = "pooling")
+bestModels$bic <- plm(as.formula(paste(y, "~", rhs_list[[best.idx.bic]])), data = pdata, model = "pooling")
 
-bestModel <- plm(as.formula(paste(y, "~", rhs_list[[best.idx]])), data = pdata, model = "pooling")
-
-result.lst <- setNames(list(best$model), best$rhs)
-cat("Best RHS:", names(result.lst), "  BIC:", min(bics), "\n")
-
-# POLS (pooled OLS) ################################################################################
-
-POLS <- plm(fml_base, data = pdata, model = "pooling")
-
+for(i in 1:length(bestModels)){
+	cat(sprintf("Best RHS by %s: %s\n", names(bestModels)[i], as.character(bestModels[[i]]$formula)[3]))
+}
 # Plot ################
+figFolder<-file.path('figures','damFacStatsSurvey',paste0('upTo',max_k,'Covariates'))
+dir.create(figFolder,showWarnings = F,recursive = T)
 cfdata <- pdata
 cfdata[,grep('sta$',colnames(cfdata))] <- 0
-damfac.lst <- list()
-years <- 2020:2149
-figFolder<-file.path('figures','damFacStatsSurvey')
-dir.create(figFolder,showWarnings = F,recursive = T)
-for(i in 1:length(result.lst)){
-  model <- result.lst[[i]]
-  pred <- predict(model)
-  cfPred <- predict(model,newdata = cfdata)
-  moddiff <- cfPred-pred
-  damfac <- moddiff/cfPred
-  damfac.lst[[names(result.lst)[i]]] <- damfac
-  png(file.path(figFolder,paste0('modelPOLS-',i,'.png')),400,400)
-  plot(c(pdata$sta),c(damfac),pch=20,
-       main=names(result.lst)[i])
-  dev.off()
+cfdata[,grep('sta2$',colnames(cfdata))] <- 0
+cfdata[,grep('sta3$',colnames(cfdata))] <- 0
+damfacs <- list()
+for(i in 1:length(bestModels)){
+	name <- names(bestModels)[i]
+	years <- 2020:2149
+	pred <- predict(bestModels[[i]])
+	cfPred <- predict(bestModels[[i]],newdata = cfdata)
+	moddiff <- cfPred-pred
+	damfacs[[name]] <- moddiff/cfPred
+	png(file.path(figFolder,paste0('modelPOLS-',name,'.png')),400,400)
+	plot(c(pdata$sta),c(damfacs[[name]]),pch=20,
+	     main=name)
+	dev.off()
 }
-
-
-
-
-
-
-
